26ee0bb49cb4e3e5b0edddfe0aa1cc2fb715c97a
Pedro Lucas <plrf100@gmail.com>
2022-06-19 23:10:20 -0300

    git(upload): uploading to github

---
+++ main.py
@@ -0,0 +1,106 @@
+import numpy as np
+import pandas as pd
+import os
+import cv2
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam
+from sklearn.metrics import accuracy_score
+np.random.seed(42)
+
+train_path = 'Train'
+test_path = 'Test'
+
+IMG_HEIGHT = 30
+IMG_WIDTH = 30
+channels = 3
+
+NUM_CATEGORIES = len(os.listdir(train_path))
+
+image_data = []
+image_labels = []
+
+for i in range(NUM_CATEGORIES):
+    path = 'Train/' + str(i)
+    images = os.listdir(path)
+
+    for img in images:
+        try:
+            image = cv2.imread(path + '/' + img)
+            image_fromarray = Image.fromarray(image, 'RGB')
+            resize_image = image_fromarray.resize((IMG_HEIGHT, IMG_WIDTH))
+            image_data.append(np.array(resize_image))
+            image_labels.append(i)
+        except:
+            print("Error in " + img)
+
+# Changing the list to numpy array
+image_data = np.array(image_data)
+image_labels = np.array(image_labels)
+
+print(image_data.shape, image_labels.shape)
+
+shuffle_indexes = np.arange(image_data.shape[0])
+np.random.shuffle(shuffle_indexes)
+image_data = image_data[shuffle_indexes]
+image_labels = image_labels[shuffle_indexes]
+
+# ister éguí
+X_train, X_val, y_train, y_val = train_test_split(image_data, image_labels, test_size=0.3, random_state=42, shuffle=True)
+
+X_train = X_train/255 
+X_val = X_val/255
+
+print("X_train.shape", X_train.shape)
+print("X_valid.shape", X_val.shape)
+print("y_train.shape", y_train.shape)
+print("y_valid.shape", y_val.shape)
+
+y_train = keras.utils.to_categorical(y_train, NUM_CATEGORIES)
+y_val = keras.utils.to_categorical(y_val, NUM_CATEGORIES)
+
+print(y_train.shape)
+print(y_val.shape)
+
+model = keras.models.Sequential([    
+    keras.layers.Conv2D(filters=16, kernel_size=(3,3), activation='relu', input_shape=(IMG_HEIGHT,IMG_WIDTH,channels)),
+    keras.layers.Conv2D(filters=32, kernel_size=(3,3), activation='relu'),
+    keras.layers.MaxPool2D(pool_size=(2, 2)),
+    keras.layers.BatchNormalization(axis=-1),
+    
+    keras.layers.Conv2D(filters=64, kernel_size=(3,3), activation='relu'),
+    keras.layers.Conv2D(filters=128, kernel_size=(3,3), activation='relu'),
+    keras.layers.MaxPool2D(pool_size=(2, 2)),
+    keras.layers.BatchNormalization(axis=-1),
+    
+    keras.layers.Flatten(),
+    keras.layers.Dense(512, activation='relu'),
+    keras.layers.BatchNormalization(),
+    keras.layers.Dropout(rate=0.5),
+    
+    keras.layers.Dense(43, activation='softmax')
+])
+
+lr = 0.001
+epochs = 30
+
+opt = Adam(lr=lr, decay=lr / (epochs * 0.5))
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+aug = ImageDataGenerator(
+    rotation_range=10,
+    zoom_range=0.15,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.15,
+    horizontal_flip=False,
+    vertical_flip=False,
+    fill_mode="nearest")
+
+history = model.fit(aug.flow(X_train, y_train, batch_size=32), epochs=epochs, validation_data=(X_val, y_val))
+
+model.save("model.h5")
---
+++ predictionTest.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import os
+import cv2
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam
+from sklearn.metrics import accuracy_score
+np.random.seed(42)
+
+IMG_HEIGHT = 30
+IMG_WIDTH = 30
+channels = 3
+
+model_path = "model.h5"
+loaded_model = tf.keras.models.load_model(model_path)
+
+image = cv2.imread("sessenta.png")
+
+image_fromarray = Image.fromarray(image, 'RGB')
+resize_image = image_fromarray.resize((30, 30))
+expand_input = np.expand_dims(resize_image,axis=0)
+input_data = np.array(expand_input)
+input_data = input_data/255
+
+pred = loaded_model.predict(input_data)
+result = pred.argmax()
+print(result)
